9449e3d82d851ba18e17e4b597b69b540874e829
Stackwipe <58694787+Stackwipe@users.noreply.github.com>
2020-09-06 20:15:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
+
 import pygame
 from pygame.locals import *
+#from tkinter import * GUI!
 import sys
-import random
 
+
+# Init pygame
 pygame.init()
 
-# wielkość okna
-size = width, height = 1280, 900
+# Window Size
+size = width, height = 900, 600
 screen = pygame.display.set_mode(size)
 pygame.mouse.set_visible(0)
 
-# tło
-background = pygame.image.load("tlo.jpg")
-background = pygame.transform.scale(background, (1280, 900))
+# Background
+background = pygame.image.load("cool.jpg")
+background = pygame.transform.scale(background, (900, 600))
 
-# Pętla gry
+# Title and Icon
+pygame.display.set_caption("Wantuch Na Kozaku")
+icon = pygame.image.load("java.png")
+pygame.display.set_icon(icon)
+
+# Game Loop
 running = True
 while running:
     pygame.display.update()
-    screen.fill((120,0,0))
+    #screen.fill((120,0,0))
 
-    #ładowanie tła
-    screen.blit(background,(0,0))
+    # Background Init
+    screen.blit(background, (0, 0))
 
-    # wyłączanie gry
+    # Game Init/Quit and Main Events
     for event in pygame.event.get():
         if event.type == QUIT:
             sys.exit()
-
-
-
-
-
