56e881e8ab12af80c567b965fd04d65d08af6a76
FWalkowicz <32652664+FWalkowicz@users.noreply.github.com>
2020-09-06 17:34:23 +0200

    first step in wantuch's game

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.locals import *
+import sys
+import random
+
+pygame.init()
+
+# wielkość okna
+size = width, height = 1280, 900
+screen = pygame.display.set_mode(size)
+pygame.mouse.set_visible(0)
+
+# tło
+background = pygame.image.load("tlo.jpg")
+background = pygame.transform.scale(background, (1280, 900))
+
+# Pętla gry
+running = True
+while running:
+    pygame.display.update()
+    screen.fill((120,0,0))
+
+    #ładowanie tła
+    screen.blit(background,(0,0))
+
+    # wyłączanie gry
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit()
+
+
+
+
+
