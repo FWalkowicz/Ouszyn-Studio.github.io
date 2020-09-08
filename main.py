e5fb4dc31465c2e90ef32f10220e709b097ab144
FWalkowicz <32652664+FWalkowicz@users.noreply.github.com>
2020-09-08 18:14:38 +0200

    move
    Co-Authored-By: fwalkowicz <32652664+fwalkowicz@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,49 +1,105 @@
 import pygame
 from pygame.locals import *
 import sys
+import os
 
 pygame.init()
 
-# wielkość okna
-size = width, height = 1280, 900
-screen = pygame.display.set_mode(size)
-pygame.mouse.set_visible(0)
+########################################################################################################################
+# properties
+########################################################################################################################
 
-# tło
+WIDTH = 1280
+HEIGHT = 900
+
+TITLE = "Wantuch's game"
+
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+# window
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# title
+pygame.display.set_caption(TITLE)
+
+# visibility of mouse
+pygame.mouse.set_visible(1)
+
+# background image
 background = pygame.image.load("tlo.jpg")
-background = pygame.transform.scale(background, (1280, 900))
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
-# Główny bohater
-class Wantuch():
+# fps
+clock = pygame.time.Clock()
+
+# player position
+player_pos = [0, 800]
+player_size = [100, 100]
+
+########################################################################################################################
+
+# character
+class Player(pygame.sprite.Sprite):
     def __init__(self):
-        size = width, height = 100, 100
-        wiktor = self.pygame.image.load("banas.png")
-        self.pygame.transform.scale(wiktor, (100,100))
-        self.rect = self.image.get_rect()
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for i in range(1, 15):
+            img = pygame.image.load(os.path.join('png', 'Walk (' + str(i) + ').png')).convert()
+            self.images.append(pygame.transform.scale(img, (player_size[0], player_size[1])))
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+            self.frame = 0
 
-        self.change_x = 0
-        self.change_y = 0
 
-        self.level = None
 
-    def update(self):
-        self.calc_grav()
+
+
+player = Player()
+player.rect.x = player_pos[0]
+player.rect.y = player_pos[1]
+
+
 
 
 
 # Pętla gry
 running = True
 while running:
-    pygame.display.update()
-    screen.fill((120,0,0))
+
+    window.fill((WHITE))
 
     #ładowanie tła
-    screen.blit(background,(0,0))
+    window.blit(background, (0, 0))
 
-    # wyłączanie gry
+
     for event in pygame.event.get():
+
+        # exit
         if event.type == QUIT:
             sys.exit()
+
+        # player movement
+        if event.type == pygame.KEYDOWN:
+            x = player_pos[0]
+            y = player_pos[1]
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                x -= 100
+            elif event.key == pygame.K_RIGHT or event.key == ord('d'):
+                x += 100
+
+            player_pos = [x, y]
+
+    pygame.draw.rect(window, RED, (player_pos[0], player_pos[1], player_size[0], player_size[1]))
+    #player.draw(window)
+
+    player.update()
+    pygame.display.update()
+
+
+
 
 
 
