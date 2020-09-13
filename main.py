accc14d504469dda44d3a6e78309cce058d68204
Skicken <45664464+Skicken@users.noreply.github.com>
2020-09-13 19:13:54 +0200

    aha
    a

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-import pygame as pg
-from pygame.locals import *
-import sys
-<<<<<<< Updated upstream
-
-pygame.init()
-
-# wielkość okna
-size = width, height = 1280, 900
-screen = pygame.display.set_mode(size)
-pygame.mouse.set_visible(0)
-
-# tło
-background = pygame.image.load("tlo.jpg")
-background = pygame.transform.scale(background, (1280, 900))
-
-# Pętla gry
-print("testuje")
-running = True
-while running:
-    pygame.display.update()
-    screen.fill((120,0,0))
-
-    #ładowanie tła
-    screen.blit(background,(0,0))
-
-    # wyłączanie gry
-    for event in pygame.event.get():
-=======
 import os
 from player import * 
 from gameSettings import *
@@ -65,21 +36,14 @@ while running:
     window.fill((WHITE))
     window.blit(background, (0, 0))
     for event in pg.event.get():
->>>>>>> Stashed changes
         if event.type == QUIT:
             sys.exit()
     player.checkColision(platforms)      
     player.handleMovment()
 
-<<<<<<< Updated upstream
-print("WAntuch na kozaku")
-=======
     all_sprites.draw(window)
     pg.display.update()
 
-
-
->>>>>>> Stashed changes
 
 
 
